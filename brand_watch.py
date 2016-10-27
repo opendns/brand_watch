5ad8c40f8dfa523c899c0a8781ebf1f235a48f3b
brad-anton <brad.antonie@gmail.com>
2016-10-27 09:50:28 -0400

    added apikey from file

--- brand_watch.py
+++ brand_watch.py
@@ -7,16 +7,23 @@ brand_watch.py
 from BrandWatch import Watcher
 import argparse
 
-api_key = None
+
+api_key = None # Define your api key here or in config.json 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('brand')
     parser.add_argument('-e', '--exclude')
     args = parser.parse_args()
+
+    if api_key is None:
+        with open('config.json', 'rb') as f:
+            from json import load
+            config = load(f)
+        api_key = config['investigate_api_key']
     
     if not args.exclude:
-        w = Watcher(api_key=api_key, brand=args.brand)
+        w = Watcher(api_key=api_key, brand=args.brand, days=30)
     else:
         with open(args.exclude, 'rb') as f:
             exclude = f.read().splitlines()
